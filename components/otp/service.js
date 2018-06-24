8c1cdb06caeac7efbb81e28fa30f53c0b9db1739
ramin <sirramin@gmail.com>
2018-06-24 18:33:10 +0430

    otp

--- components/otp/service.js
+++ components/otp/service.js
@@ -1,5 +1,14 @@
-const checkUserExists = ()=>{
-
+rpn = require('request-promise-native')
+const checkUserExists = async (phoneNumber) => {
+    const options = {
+        method: 'POST',
+        uri: 'https://landing.artatel.ir/api/otp/v1/confirmation/masterOfMind/' + phoneNumber,
+        body: {
+            "verificationCode":
+        },
+        json: true // Automatically stringifies the body to JSON
+    };
+    await rpn.post(options)
 }
 module.exports = {
     checkUserExists: checkUserExists
