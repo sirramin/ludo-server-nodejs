b1afe06db9dd407ae29961fd72e0b1fd76ded21f
ramin <sirramin@gmail.com>
2019-10-15 02:13:39 +0330

    test binary

--- components/user/route.js
+++ components/user/route.js
@@ -2,6 +2,10 @@ const router = require('express').Router()
 const service = require('./service')
 const Boom = require('@hapi/boom')
 
+const flatbuffers = require('../../flatBuffers/flatbuffers').flatbuffers
+const builder = new flatbuffers.Builder(0)
+const ReposList = require('../../flatBuffers/schemas/test_generated').ReposList
+
 router.post('/signInAsGuest', async (req, res, next) => {
   // throw Boom.badRequest('test')
   const tokenAndUserId = await service.registerGuestUser()
@@ -11,6 +15,16 @@ router.post('/signInAsGuest', async (req, res, next) => {
     userId: tokenAndUserId.userId
   })
 
+})
+
+router.post('/testFlatBuffer', async (req, res, next) => {
+  const name = builder.createString('Ramin')
+  ReposList.startReposList(builder)
+  ReposList.addName(builder, name)
+  const repo = ReposList.endReposList(builder)
+  builder.finish(repo)
+  const buf = builder.asUint8Array()
+  res.send(buf)
 })
 
 module.exports = router
