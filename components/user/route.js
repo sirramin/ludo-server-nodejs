e98d20a80e7a075bed189c2146d365555c3f08d2
ramin <sirramin@gmail.com>
2019-10-17 01:52:46 +0330

    sing in as guest flat buffers

--- components/user/route.js
+++ components/user/route.js
@@ -2,22 +2,31 @@ const router = require('express').Router()
 const service = require('./service')
 const Boom = require('@hapi/boom')
 
-const flatbuffers = require('../../flatBuffers/flatbuffers').flatbuffers
+const {flatbuffers} = require('../../flatBuffers/flatbuffers')
 const builder = new flatbuffers.Builder(0)
-const ReposList = require('../../flatBuffers/schemas/test_generated').ReposList
+const {ReposList} = require('../../flatBuffers/schemas/test_generated')
+const {signInAsGuest} = require('./schemas/singInAsGuest_generated')
 
 router.post('/signInAsGuest', async (req, res, next) => {
   // throw Boom.badRequest('test')
-  const tokenAndUserId = await service.registerGuestUser()
+  const {token, userId} = await service.registerGuestUser()
   // await gameServiceObj.insertUserGameData(tokenAndUserId.userId)
-  res.send({
-    token: tokenAndUserId.token,
-    userId: tokenAndUserId.userId
-  })
-
+  // res.send({
+  //   token: token,
+  //   userId: userId
+  // })
+  const tokenString = builder.createString(token)
+  const userIdString = builder.createString(userId)
+  signInAsGuest.startsignInAsGuest(builder)
+  signInAsGuest.addToken(builder, tokenString)
+  signInAsGuest.addUserId(builder, userIdString)
+  const repo = signInAsGuest.endsignInAsGuest(builder)
+  builder.finish(repo)
+  const buf = builder.asUint8Array()
+  res.send(buf)
 })
 
-router.post('/testFlatBuffer', async (req, res, next) => {
+router.get('/testFlatBuffer', async (req, res, next) => {
   const name = builder.createString('Ramin')
   ReposList.startReposList(builder)
   ReposList.addName(builder, name)
--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -2,6 +2,7 @@ const errorHandler = (err, req, res, next) => {
   if (err.isBoom) {
     res.status(err.output.statusCode).json({error: err.message})
   } else {
+    logger.error(err.message)
     res.status(500).json({error: 'internal server error'})
   }
 }
