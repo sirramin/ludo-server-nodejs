0fa3925de6378e8bb09c2bbe39f247da123bfbf2
ramin <sirramin@gmail.com>
2019-10-24 03:19:39 +0330

    error event playerAlreadyJoined

--- components/matchMaking/matchMaking.js
+++ components/matchMaking/matchMaking.js
@@ -4,7 +4,7 @@ const redisHelperUser = require('../redisHelper/user')
 const findAvailableRooms = async (socket) => {
   const isPlayerJoinedBefore = await redisHelperUser.findUserCurrentRoom(socket.userId)
   if (isPlayerJoinedBefore) {
-    socket.emit('matchEvent', 'playerAlreadyJoined')
+    socket.emit('error', 'playerAlreadyJoined')
     return
   }
   let roomId = await loopOverAllRooms(null)
