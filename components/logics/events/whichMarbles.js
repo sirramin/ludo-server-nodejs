938f5aac04ebef0d11b5e2cd5f799c273494bde9
ramin <sirramin@gmail.com>
2019-11-23 06:13:06 +0330

    whichMarblesCanMove refactor

--- components/logics/events/whichMarbles.js
+++ components/logics/events/whichMarbles.js
@@ -12,41 +12,43 @@ const whichMarblesCanMove = async (roomId) => {
   let marblesCantMove = []
   const currentPlayerMarbles = marblesPosition[currentPlayer]
 
-  currentPlayerMarbles.forEach((marblePosition, index) => {
+  for (const [index, marblePosition] of currentPlayerMarbles.entries()) {
+
     const currentMarbleNumber = index + 1
     const newPosition = await positionCalculator(roomId, marblePosition, diceNumber)
 
     if (!newPosition) {
-      marblesCantMove = _.union(marblesCantMove, [currentMarbleNumber]);
+      marblesCantMove = _.union(marblesCantMove, [currentMarbleNumber])
     }
 
     if (diceNumber !== 6 && marblePosition === 0) {
-      marblesCantMove = _.union(marblesCantMove, [currentMarbleNumber]);
+      marblesCantMove = _.union(marblesCantMove, [currentMarbleNumber])
     }
 
     if (tileStarts.indexOf(newPosition) !== -1) {
-      marblesCantMove = _tileStartConflict(newPosition, marblesPosition)
+      marblesCantMove = _tileStartConflict(newPosition, marblesPosition, marblesCantMove, currentMarbleNumber)
     }
 
-      currentPlayerMarbles.forEach((marblePosition2, marbleNumber2) => {
-        // player marble cant sit on same color
-        // diceNumber === 6 && marblePosition === 0 &&
-        if (marbleNumber2 + 1 !== currentMarbleNumber && newPosition === marblePosition2 && marblePosition2 !== 0)
-          marblesCantMove = _.union(marblesCantMove, [currentMarbleNumber]);
+    for (const [index2, marblePosition2] of currentPlayerMarbles.entries()) {
 
-        //player has not enough space in its last tiles
-        if (marblePosition !== 0 && newPosition >= tilesStartEndLastCurrentPlayer[2]) {
-          if (marblePosition2 >= tilesStartEndLastCurrentPlayer[2] && marblePosition2 <= tilesStartEndLastCurrentPlayer[3])
-            if (newPosition === marblePosition2)
-              marblesCantMove = _.union(marblesCantMove, [currentMarbleNumber]);
-          logger.info('---------5--------')
+      if (index2 + 1 !== currentMarbleNumber && newPosition === marblePosition2 && marblePosition2 !== 0) {
+        marblesCantMove = _conflictSameColor(marblesCantMove, currentMarbleNumber)
+      }
+
+      if (marblePosition2 >= tilesStartEndLastCurrentPlayer[2] && marblePosition2 <= tilesStartEndLastCurrentPlayer[3]) {
+        if (newPosition === marblePosition2) {
+          marblesCantMove = _conflictInGateWay(marblesCantMove, currentMarbleNumber)
         }
-      })
-  })
+      }
+
+    }
+
+  }
+
   return _.difference([1, 2, 3, 4], marblesCantMove)
 }
 
-const _tileStartConflict = async (newPosition, marblesPosition) => {
+const _tileStartConflict = async (newPosition, marblesPosition, marblesCantMove, currentMarbleNumber) => {
   // checking other marbles in their starting tile conflict
   // if this current player marble target meet one of the tileStarts
   const targetPlayerIndex = tileStarts.indexOf(newPosition)
@@ -59,7 +61,19 @@ const _tileStartConflict = async (newPosition, marblesPosition) => {
       }
     })
   }
+}
 
+const _conflictSameColor = async (marblesCantMove, currentMarbleNumber) => {
+  // player marble cant sit on same color
+  // diceNumber === 6 && marblePosition === 0 &&
+  logger.info('---------5--------')
+  return _.union(marblesCantMove, [currentMarbleNumber])
+}
+
+const _conflictInGateWay = async (marblesCantMove, currentMarbleNumber) => {
+  //player has not enough space in its last tiles
+  logger.info('---------6--------')
+  return _.union(marblesCantMove, [currentMarbleNumber])
 }
 
 
