c4265cbe645c7461059fedb0a2f8304cb746dace
ramin <sirramin@gmail.com>
2020-01-17 18:32:51 +0330

    mongo replication

--- common/mongoose-client.js
+++ common/mongoose-client.js
@@ -1,6 +1,10 @@
 const mongooseClient = require('mongoose')
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost'
-const connections = mongooseClient.connect(mongoUrl + '/mench', {useNewUrlParser: true})
+const connections = mongooseClient.connect(mongoUrl + '/mench',
+  {
+    useNewUrlParser: true,
+    autoReconnect: true
+  })
 // connections.on('error', console.error.bind(console, 'connection error: '))
 // connections.once('open', function () {
 //   logger.info('mongoose connected')
