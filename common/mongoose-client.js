20a5f0cec998c93f16f5cbaaa368422b2cc364a6
ramin <sirramin@gmail.com>
2020-01-21 01:45:43 +0330

    useUnifiedTopology

--- common/mongoose-client.js
+++ common/mongoose-client.js
@@ -1,10 +1,11 @@
 const mongooseClient = require('mongoose')
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost'
-const connections = mongooseClient.connect(mongoUrl + '/mench?replicaSet=replicaset',
-  {
-    useNewUrlParser: true,
-    autoReconnect: true
-  })
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/mench'
+const connections = mongooseClient.connect(mongoUrl,
+    {
+        useNewUrlParser: true,
+        autoReconnect: true,
+        useUnifiedTopology: true
+    })
 // connections.on('error', console.error.bind(console, 'connection error: '))
 // connections.once('open', function () {
 //   logger.info('mongoose connected')
